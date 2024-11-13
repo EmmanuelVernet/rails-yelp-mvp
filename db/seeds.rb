b0725e977164ab2455c445957174ebfbb8e90289
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2024-11-13 15:23:27 +0100

    added MVC for restaurants and review routing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "clearing seeds"
+Restaurant.destroy_all
+
+puts "creating 5 restaurants"
+Restaurant.create(name: "Bouillon Chartier", address: "Paris 9eme", category: "french", phone_number: "111111111")
+Restaurant.create(name: "Bouillon Pigalle", address: "Paris 18eme", category: "french", phone_number: "122222221")
+Restaurant.create(name: "Epicure", address: "somewhere", category: "french", phone_number: "133333331")
+Restaurant.create(name: "La Tour d'Argent", address: "Tour Eiffel", category: "french", phone_number: "114444441")
+Restaurant.create(name: "Bouillon Racine", address: "Paris 6eme", category: "french", phone_number: "155555551")
+
+puts "#{Restaurant.count} Restaurants created!"
